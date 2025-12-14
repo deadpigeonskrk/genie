257af8099f6a90c390a864f0c56e910baf980afb
Caroline Grand-Clement <187895937+caroloading@users.noreply.github.com>
2025-12-14 13:50:28 +0100

    tinker main

--- Lab5.hs
+++ Lab5.hs
@@ -1,3 +1,10 @@
+module Lab5
+where 
+import System.IO
+import System.IO.Error(tryIOError)
+
+
+-- | Data type
 type Person   = String
 type Question = String
 
@@ -5,8 +12,7 @@ type Question = String
 data QA = P Person | Q Question QA QA
     deriving (Read,Show)
 
-
-
+-- | Utilities 
 yesAnswer :: String
 yesAnswer = "yes"
 noAnswer :: String
@@ -22,26 +28,57 @@ writeQFile :: QA -> IO ()
 writeQFile qa = writeFile "questions.qa" (show qa)
 
 
+-- | Default data
 defaultData :: QA
 defaultData = Q "Is this person from Europe?" 
-    (Q "Is this person a scientist?" (P "Marie Skłodowska-Curie") (P "Queen Elisabeth II")) 
-    (Q "Is this person an actor?" (P "Marlin Monroe") (P "Hillary Clinton"))
+    (Q "Is this person a scientist?" 
+        (P "Marie Skłodowska-Curie") 
+        (P "Queen Elisabeth II")) 
+    (Q "Is this person an actor?" 
+        (P "Marilyn Monroe") 
+        (P "Hillary Clinton"))
 
-beta :: QA -> IO()
-beta (Q q y n) = do
-                putStrLn q
-                answ <- getLine 
+
+-- | Ask questions recursively until program has a guess 
+-- | returns person guessed & answer
+askQ :: QA -> IO (String, String)
+askQ (Q q y n) = do
+                    putStrLn q
+                    answ <- getLine 
+                    checkAn answ
+                    where checkAn a 
+                            | yesAnswer == a = askQ y
+                            | noAnswer  == a = askQ n
+                            | otherwise      = do
+                                        putStrLn "I recognise only 'yes' or 'no' answer"
+                                        askQ (Q q y n)
+askQ (P p)     = do
+                putStrLn $ "My guess: Is it " ++ p ++ "?"
+                answ <- getLine
                 checkAn answ
                 where checkAn a 
-                                |yesAnswer == a = beta y
-                                |noAnswer  == a = beta n
-                                |otherwise      = error "I recognise only 'yes' or 'no' answer"
-beta (P p)     = do
-                 putStrLn $ "My guess: Is it " ++ p ++ "?"
-                 answ <- getLine
-                 checkAn answ
-                where checkAn a 
-                                |yesAnswer == a = putStrLn "I won"
-                                |noAnswer  == a = putStrLn "You won"
-                                |otherwise      = error "I recognise only 'yes' or 'no' answer"
+                        | yesAnswer /= a && noAnswer /= a = 
+                            putStrLn "I recognise only 'yes' or 'no' answer" >> askQ (P p)
+                        | otherwise = return (p, a)
  
+main :: IO ()
+main = do
+    questions <- getData
+    result <- askQ questions
+    updatedquestions <- checkAns result questions
+    writeQFile questions
+    where checkAns (lastGuess, answer) questions 
+                    | yesAnswer == answer = 
+                       putStrLn "I won" 
+                       >> return playAgain
+                    | noAnswer  == answer =
+                       putStrLn "OK - you won this time." 
+                       >> return (addInfo questions lastGuess)
+
+playAgain = defaultData
+
+addInfo :: QA -> Person -> QA
+addInfo = undefined
+
+getData :: IO QA
+getData = return defaultData
